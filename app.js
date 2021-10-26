b44164ad3d9b8f7c7d35dcfa912cdbe1dd5e0171
TP <phamtanphat1294@gmail.com>
2021-10-26 14:36:04 -0400

    Add another page with its own list

--- app.js
+++ app.js
@@ -5,35 +5,58 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
   // New Way to use the switch
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
 });
 
-app.post("/", function(req,res){
-  var item = req.body.newItem;
+app.post("/", function(req, res) {
 
-  items.push(item);
+  let item = req.body.newItem;
 
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else{
+    items.push(item);
+    res.redirect("/");
+  }
 })
 
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+})
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 app.listen(3000, function() {
   console.log("Server started and listening on port 3000");
 });
