f0aaf2ab22ddc39135f5af705034d2fc46b16ae0
TP <phamtanphat1294@gmail.com>
2022-03-03 17:44:22 -0500

    reformat the code using mangoose and adding mongoDB as database

--- app.js
+++ app.js
@@ -2,24 +2,65 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
 const app = express();
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
+// Connect to mongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+// Mongoose section
+const itemSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todo list!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
 app.get("/", function(req, res) {
-
-  let day = date.getDate();
-
-  res.render("list", {listTitle: day,newListItems: items});
+  // mongoose find() -> {} mean finds all
+  Item.find({}, function(err, foundItems){
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err);
+        } else {
+          console.log("Default item inserted successfully!");
+        }
+      });
+      // Redirect here goes back to the beginning of the app.get section
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+  });
 });
 
 app.post("/", function(req, res) {
@@ -29,14 +70,17 @@ app.post("/", function(req, res) {
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
-  } else{
+  } else {
     items.push(item);
     res.redirect("/");
   }
 })
 
 app.get("/work", function(req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems});
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 })
 
 app.post("/work", function(req, res) {
@@ -45,7 +89,7 @@ app.post("/work", function(req, res) {
   res.redirect("/work");
 })
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 })
 app.listen(3000, function() {
